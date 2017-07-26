e6336a2479e117e415393b3e950a73acc7c9cf84
Arthur White <arthur@whitedevops.com>
2017-07-26 22:23:07 +0200

    Simplify watch event handling

--- cmd/gowww/watch.go
+++ cmd/gowww/watch.go
@@ -38,6 +38,15 @@ func watcherAdd(dir string) {
 	}
 }
 
+func eventIs(e fsnotify.Event, ops ...fsnotify.Op) bool {
+	for _, op := range ops {
+		if e.Op&op == op {
+			return true
+		}
+	}
+	return false
+}
+
 func watch() {
 	initWatcher()
 	if buildGo() == nil {
@@ -45,13 +54,8 @@ func watch() {
 	}
 	for {
 		select {
-		case event := <-watcher.Events:
-			if event.Op&fsnotify.Create == fsnotify.Create ||
-				event.Op&fsnotify.Write == fsnotify.Write ||
-				event.Op&fsnotify.Remove == fsnotify.Remove ||
-				event.Op&fsnotify.Rename == fsnotify.Rename {
-				watchEvent(event.Name)
-			}
+		case e := <-watcher.Events:
+			watchEvent(e)
 		case err := <-watcher.Errors:
 			if err != nil {
 				panic(err)
@@ -60,23 +64,26 @@ func watch() {
 	}
 }
 
-func watchEvent(name string) {
-	if strings.HasPrefix(name, "scripts/") {
-		if strings.HasSuffix(name, ".go") && !strings.HasSuffix(name, "_test.go") {
+func watchEvent(e fsnotify.Event) {
+	if eventIs(e, fsnotify.Chmod) {
+		return
+	}
+	if strings.HasPrefix(e.Name, "scripts/") {
+		if strings.HasSuffix(e.Name, ".go") && !strings.HasSuffix(e.Name, "_test.go") {
 			buildScriptsGopherJS()
 		}
 		// TODO: Babel, CoffeeScript, TypeScript...
-	} else if strings.HasPrefix(name, "styles/") &&
-		!strings.Contains(name, "mixin") &&
-		!strings.Contains(name, "partial") &&
-		filepath.Base(name)[0] != '_' {
-		if strings.HasSuffix(name, ".styl") {
-			buildStylesStylus(name)
+	} else if strings.HasPrefix(e.Name, "styles/") &&
+		!strings.Contains(e.Name, "mixin") &&
+		!strings.Contains(e.Name, "partial") &&
+		filepath.Base(e.Name)[0] != '_' {
+		if strings.HasSuffix(e.Name, ".styl") {
+			buildStylesStylus(e.Name)
 		}
 		// TODO: LESS, SASS, SCSS...
-	} else if strings.HasPrefix(name, "views/") {
+	} else if strings.HasPrefix(e.Name, "views/") {
 		run()
-	} else if strings.HasSuffix(name, ".go") && !strings.HasSuffix(name, "_test.go") {
+	} else if strings.HasSuffix(e.Name, ".go") && !strings.HasSuffix(e.Name, "_test.go") {
 		if buildGo() == nil {
 			run()
 		}
